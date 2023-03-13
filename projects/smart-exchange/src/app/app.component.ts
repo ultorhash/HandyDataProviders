fed1242f33abcb774e7629ebd6417d612c896b64
ultor <ultorhash@gmail.com>
2023-03-13 18:07:46 +0100

    remove dead code

--- projects/smart-exchange/src/app/app.component.ts
+++ projects/smart-exchange/src/app/app.component.ts
@@ -7,8 +7,7 @@ import { UpdateCoins } from './store';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  templateUrl: './app.component.html'
 })
 export class AppComponent implements OnInit {
   constructor(
